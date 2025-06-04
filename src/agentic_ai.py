5371269ae64e2551d215c4fef1d88bb0cc130a1e
Hieu Tran <jrvn-hieu@Hieus-MacBook-Pro.local>
2025-06-04 23:49:51 +0700

    add demo

--- src/agentic_ai.py
+++ src/agentic_ai.py
@@ -8,3 +8,17 @@ if __name__ == "__main__":
 
     with open("langgraph_diagram.png", "wb") as f:
         f.write(png_bytes)
+
+    graph_qa_result = graph_app.invoke(
+        {"question": "find top 5 cited articles and return their title"}
+    )
+
+    print(graph_qa_result['documents'])
+    result = graph_app.invoke(
+        {"question": "find articles about oxidative stress. Return the title of the most relevant article"}
+    )
+
+    print(result.keys())
+    print(result['subqueries'])
+    print(result['documents'])
+
